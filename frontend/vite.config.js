a0f3c7374a8f2470dc49a9143f659e40d1db36b5
Sayantansen123 <riosayan123@gmail.com>
2024-12-09 15:00:43 +0530

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'https://meChat.onrender.com/',
         secure: false
       }
     }
